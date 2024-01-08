9b6baaa6a83288122819d5c0eb5eb3d43bbe9a4f
m13ha <nwaokocham@gmail.com>
2024-01-08 17:42:59 +0100

    side bar functionality included

--- src/componenets/SideBar.tsx
+++ src/componenets/SideBar.tsx
@@ -1,17 +1,40 @@
 import * as React from 'react';
+import { json, useLocation } from 'react-router-dom';
+import sideBarInfo  from "../assets/sideBarInfo.json"
 
 interface ISideBarProps {
 }
 
-const SideBar: React.FunctionComponent<ISideBarProps> = (props) => {
+const SideBar: React.FunctionComponent<ISideBarProps> = () => {
+  let location = useLocation();
+  const [sideBarText, setSideBarText] = React.useState(() => {
+    return sideBarInfo[location.pathname as keyof typeof sideBarInfo].text || ""
+  })
+  const [sideBarLink, setSideBarLink] = React.useState(() => {
+    return sideBarInfo[location.pathname as keyof typeof sideBarInfo].link || ""
+  })
+  const [sideBarCode, setSideBarCode] = React.useState("")
+
+
+  React.useEffect(() => {
+    setSideBarText(sideBarInfo[location.pathname as keyof typeof sideBarInfo].text || "")
+    setSideBarLink(sideBarInfo[location.pathname as keyof typeof sideBarInfo].link || "")
+  },[location]);
+
+
   return (
     <>
-        <div>code</div>
-        <div>How it works</div>
-        <div>
-            <button>code</button>
-            <button>How it works</button>
-        </div>
+      <div>
+        <p>{sideBarText}</p>
+        <a href={sideBarLink} target='_'>Learn More</a>
+      </div>
+      <div>
+        <p>{sideBarCode}</p>
+      </div>
+      <div>
+        <button>code</button>
+        <button>How it works</button>
+      </div>
     </>
   );
 };
